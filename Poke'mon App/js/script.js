8526c1c8374ae51c7fc42cdb47ead633704081d3
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2023-03-30 16:24:33 -0400

    Add files via upload

---
+++ Poke'mon App/js/script.js
@@ -0,0 +1,45 @@
+let pokemon_List= [0,1,2,3];
+	
+	pokemon_List[0]=
+	{
+	name:'Charizard',
+	height: '67',
+	category: 'Frame',
+	abilities: 'Blaze',
+	weight: '199.5 pounds'
+	}
+
+	pokemon_List[1]=
+	{
+	name:'Pikachu',
+	height: '16',
+	category: 'Mouse',
+	abilities: 'Static',
+	weight: '13.2 pounds'
+	}
+
+	pokemon_List[2]=
+	{
+	name:'Squirtle',
+	height: '20',
+	category: 'Tiny Turtle',
+	abilities: 'Torrent',
+	weight: '19.8 pounds'
+	}
+
+	pokemon_List[3]=
+	{
+	name:'Raichu',
+	height: '31',
+	category: 'Mouse',
+	abilities: 'Static',
+	weight: '66.1 pounds'
+	}
+
+	for (let i = 0; i < pokemon_List.length; i++) {
+         if (pokemon_List[i].height > 50) {
+                 document.write(`${pokemon_List[i].name} (height: ${pokemon_List[i].height} inches) - Wow! That's a big Poke'mon!`+ '<br>'+ '<br>');
+         } else {
+            document.write(`${pokemon_List[i].name} (height: ${pokemon_List[i].height} inches)`+ '<br>'+ '<br>');
+         } 
+  }
