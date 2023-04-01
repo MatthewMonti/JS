af9c83453c71490986dccbe04192dfbebbceab83
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2023-04-01 15:19:48 -0400

    rest proper format for height in array

--- Poke'mon App/js/script.js
+++ Poke'mon App/js/script.js
@@ -3,7 +3,7 @@ let pokemon_List= [];
 	pokemon_List[0]=
 	{
 	name:'Charizard',
-	height: '67 inches',
+	height: 67,
 	category: 'Frame',
 	abilities: 'Blaze',
 	weight: '199.5 pounds'
@@ -12,7 +12,7 @@ let pokemon_List= [];
 	pokemon_List[1]=
 	{
 	name:'Pikachu',
-	height: '16 inches',
+	height: 16,
 	category: 'Mouse',
 	abilities: 'Static',
 	weight: '13.2 pounds'
@@ -21,7 +21,7 @@ let pokemon_List= [];
 	pokemon_List[2]=
 	{
 	name:'Squirtle',
-	height: '20 inches',
+	height: 20,
 	category: 'Tiny Turtle',
 	abilities: 'Torrent',
 	weight: '19.8 pounds'
@@ -30,7 +30,7 @@ let pokemon_List= [];
 	pokemon_List[3]=
 	{
 	name:'Raichu',
-	height: '31 inches',
+	height: 31,
 	category: 'Mouse',
 	abilities: 'Static',
 	weight: '66.1 pounds'
