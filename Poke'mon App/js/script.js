55665280e2e24164b90fd782115aab2ad1880834
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2023-04-22 12:18:07 -0400

    Update script.js

--- Poke'mon App/js/script.js
+++ Poke'mon App/js/script.js
@@ -1,25 +1,24 @@
 let pokemonRepository= (function(){
 	let pokemonList= [
-{
 	name: "Bulbasaur",
 	height: 28,
 	types:["Overgrow"]
-},
-{
+	,
+[
 	name: "Squirtle",
 	height: 20,
 	types:["Torrent"]
-},
-{
+],
+[
 	name: "Pidgey",
 	height: 12,
 	types:["Keen Eye", "Tangled Feet"]
-},
-{
+],
+[
 	name:"Butterfree",
 	height:43,
 	types:["Compound Eyes"]
-}
+]
 ];
 return{
 	"name":name,
