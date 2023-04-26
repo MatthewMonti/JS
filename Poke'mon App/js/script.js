4a2710fb15c05e9d0d50238268a271f982015eaf
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2023-04-26 17:21:06 -0400

    test?

--- Poke'mon App/js/script.js
+++ Poke'mon App/js/script.js
@@ -1,61 +1,61 @@
 let pokemonRepository= (function(){
 	let pokemonList= [
-{
-	name: "Bulbasaur",
-	height: "28",
-	types: ["Overgrow"]
-},
-{
-	name: "Squirtle",
-	height: "20",
-	types:["Torrent"]
-},
-{
-	name: "Pidgey",
-	height: "12",
-	types: ["Keen Eye", "Tangled Feet"]
-},
-{
-	name: "Butterfree",
-	height:"43",
-	types: ["Compound Eyes"]
-}];
-function add(pokemon) {
-	if (
-		typeof pokemon === "object" &&
-		"name" in pokemon &&
-		"height" in pokemon &&
-		"types" in pokemon
-	) {
-	pokemonList.push(pokemon);
-	} else {
-		console.log("pokemon is not correct");
+		{
+			name: "Bulbasaur",
+			height: "28",
+			types: ["Overgrow"]
+		},
+		{
+			name: "Squirtle",
+			height: "20",
+			types:["Torrent"]
+		},
+		{
+			name: "Pidgey",
+			height: "12",
+			types: ["Keen Eye", "Tangled Feet"]
+		},
+		{
+			name: "Butterfree",
+			height:"43",
+			types: ["Compound Eyes"]
+		}];
+	function add(pokemon) {
+		if (
+			typeof pokemon === "object" &&
+			"name" in pokemon &&
+			"height" in pokemon &&
+			"types" in pokemon
+		) {
+		pokemonList.push(pokemon);
+		} else {
+			console.log("pokemon is not correct");
+		}
 	}
-}
-function addlistitem(pokemon){
-	let pokemon_array = document.querySelector(".pokemon-list");
-	let pokemon_item = document.createElement("li");
-	let button = document.createElement("button");
-	button.innerText = pokemon.name;
-	button.classList.add("button_style");
-	pokemon_item.appendChild(button);
-	pokemon_array.appendChild(pokemon_item);
-}
+	function addlistitem(pokemon){
+		let pokemon_array = document.querySelector(".pokemon-list");
+		let pokemon_item = document.createElement("li");
+		let button = document.createElement("button");
+		button.innerText = pokemon.name;
+		button.classList.add("button_style");
+		pokemon_item.appendChild(button);
+		pokemon_array.appendChild(pokemon_item);
+	}
 
-function showDetails (pokemon) {
-button.addEventListener('click', function (event) {
-console.log(event);
-});
+	function showDetails (pokemon) {
+		button.addEventListener('click', function (event) {
+		console.log(event);
+	});
 
 
-function getAll() {
-return pokemonList;
-}
-return {
-add:add,
-getAll:getAll,
-addlistitem: addlistitem
-};
+	function getAll() {
+		return pokemonList;
+	}
+	return {
+		add:add,
+		getAll:getAll,
+		addlistitem: addlistitem
+	}
 	
 })();
 
