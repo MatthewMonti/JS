b5bdf3db43b8f0c76cb785f1de4f7ed393563b38
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2023-04-29 19:15:58 -0400

    Update script.js

--- Poke'mon App/js/script.js
+++ Poke'mon App/js/script.js
@@ -25,9 +25,11 @@ let pokemonRepository= (function(){
 	}
 
 	function loadList(){
+		function showLoadingMessage();
 		return fetch(apiUrl).then(function (response) {
 			return response.json();
 		}).then (function (json) {
+			function hideLoadingMessage()
 			json.results.forEach(function (item) {
 				let pokemon = {
 					name: item.name,
@@ -37,20 +39,24 @@ let pokemonRepository= (function(){
 				console.log(pokemon);
 			});
 		}).catch(function (e) {
+			function hideLoadingMessage()
 				console.error(e);
 		});
 	}
 
 	function loadDetails(item) {
+		function showLoadingMessage();
 		let url = item.detailsUrl;
 		return fetch(url).then(function (response) {
 			return response.json();
 		}).then(function (details) {
+			function hideLoadingMessage();
 			//Now we add details to item
 			item.imageUrl = details.sprites.font_default;
 			item.height = details.height;
 			item.types = details.types;
 		}).catch(function (e) {
+			function hideLoadingMessage();
 			console.error(e);
 		});
 	}
