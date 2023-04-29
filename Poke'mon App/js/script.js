2652744421f17c9d0ce6cf1e88a192fd3ce34137
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2023-04-29 14:29:26 -0400

    Update Repository Function

--- Poke'mon App/js/script.js
+++ Poke'mon App/js/script.js
@@ -1,25 +1,6 @@
 let pokemonRepository= (function(){
-	let pokemonList= [
-		{
-			name: "Bulbasaur",
-			height: "28",
-			types: ["Overgrow"]
-		},
-		{
-			name: "Squirtle",
-			height: "20",
-			types:["Torrent"]
-		},
-		{
-			name: "Pidgey",
-			height: "12",
-			types: ["Keen Eye", "Tangled Feet"]
-		},
-		{
-			name: "Butterfree",
-			height:"43",
-			types: ["Compound Eyes"]
-		}];
+	let pokemonList= [];
+	let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 	function add(pokemon) {
 		if (
 			typeof pokemon === "object" &&
