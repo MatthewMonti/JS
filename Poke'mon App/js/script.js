6170db83f5f465a1ef5b651ca0e2c32f113f6bc5
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2023-05-01 19:42:08 -0400

    Update script.js

--- Poke'mon App/js/script.js
+++ Poke'mon App/js/script.js
@@ -81,7 +81,6 @@ let pokemonRepository= (function(){
 		let button = document.createElement("button");
 		button.innerText = pokemon.name;
 		button.classList.add("button_style");
-		button.idList.add('#show-modal');
 		pokemon_item.appendChild(button);
 		pokemon_array.appendChild(pokemon_item);
 		button.addEventListener("click", function(event) {
