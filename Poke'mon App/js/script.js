2327ab42988a48d8f8e7d429a70581e1fedcc702
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2023-05-01 17:57:01 -0400

    Update script.js

--- Poke'mon App/js/script.js
+++ Poke'mon App/js/script.js
@@ -50,7 +50,7 @@
     }
   });
 
-  document.querySelector('.button_style').addEventListener('click', () => {
+  document.querySelector('li').addEventListener('click', () => {
     showModal('Modal title', 'This is the modal content!');
   });
 
