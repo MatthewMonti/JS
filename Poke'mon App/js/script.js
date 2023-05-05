aaf68edc5d750a5e0c4a43426f60f3170e6d472c
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2023-05-05 14:25:44 -0400

    remove class id

--- Poke'mon App/js/script.js
+++ Poke'mon App/js/script.js
@@ -79,7 +79,6 @@ let pokemonRepository= (function(){
 		button.classList.add("button_style");
 		pokemon_item.appendChild(button);
 		pokemon_array.appendChild(pokemon_item);
-		button.classId.add('show-modal');
 		button.addEventListener("click", function(event) {
 			showModal('Modal title', 'This is the modal content!');
 		});
