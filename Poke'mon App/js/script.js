04025a44c1640b324f2ca4d0a5564c440b810e48
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2023-05-12 19:27:34 -0400

    test new version of button

--- Poke'mon App/js/script.js
+++ Poke'mon App/js/script.js
@@ -17,6 +17,7 @@ let pokemonRepository= (function(){
 		let button = document.createElement("button");
 		button.innerText = pokemon.name;
 		button.classList.add("button_style");
+		button.idList.add('modal-container')
 		pokemon_item.appendChild(button);
 		pokemon_array.appendChild(pokemon_item);
 		button.addEventListener("click", function(event) {
@@ -85,3 +86,88 @@ pokemonRepository.loadList().then(function() {
 	});
 });
 
+(function() {
+
+  let modalContainer = document.querySelector('#modal-container');
+
+
+  
+  function showModal(title, text) {
+    // Clear all existing modal content
+    modalContainer.innerHTML = '';
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    // Add the new modal content
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+
+    closeButtonElement.innerText = 'Close';
+
+    //ADD EVENTLISTNER TO CLOSE BUTTON
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = text;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add('is-visible');
+
+  // Return a promise that resolves when confirmed, else rejects
+  return new Promise((resolve, reject) => {
+    cancelButton.addEventListener('click', () => {
+      hideModal();
+      reject();
+    });
+    confirmButton.addEventListener('click', () => {
+      hideModal();
+      resolve();
+    })
+  });
+}
+
+
+
+  
+  //close button event listener MODAL CLASS
+  document.querySelector('#show-modal').addEventListener('click', () => {
+    showModal('Modal title', 'This is the modal content!');
+  });
+
+  //EVENT LISTNER ESC
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
+
+  //EVENT LISTENER CLICK OUTSIDE MODAL CLOSES MODAL
+  modalContainer.addEventListener('click', (e) => {
+    // Since this is also triggered when clicking INSIDE the modal
+    // We only want to close if the user clicks directly on the overlay
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
+
+  let dialogPromiseReject;
+  //This can be set later, by the showDialog
+  
+  function hideModal() {
+    let modalContainer = document.querySelector('#modal-container');
+    modalContainer.classList.remove('is-visible');
+
+    if (dialogPromiseReject) {
+      dialogPromiseReject();
+      dialogPromiseReject = null;
+    }
+  }
+})();
