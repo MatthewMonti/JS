cd4b4efea7ca01494d3f37feb5c84af7604495e8
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2023-05-23 18:00:57 -0400

    fix error 3

--- Poke'mon App/js/script.js
+++ Poke'mon App/js/script.js
@@ -14,7 +14,6 @@ let pokemonRespository = (function () {
     function addListItem(pokemon) {
         let pokemonList = document.querySelector(".list-group");
         let listpokemon = document.createElement("li");
-        li.classList.add("list-group-item");
         let button = document.createElement("button");
         button.innerText = pokemon.name;
         button.classList.add("btn-primary");
