1cff14a7d0cf8127a9d36f6e693c3497c65e1b4c
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2023-05-25 19:48:59 -0400

    test 1 vanilla js class add fix?

--- Pokemon-Bootstrap/Poke'mon App/js/script.js
+++ Pokemon-Bootstrap/Poke'mon App/js/script.js
@@ -13,9 +13,9 @@ let pokemonRespository = (function () {
     }
     function addListItem(pokemon) {
         let pokemonList = document.querySelector(".list-group");
-        pokemonList.addClass("row");
+        pokemonList.classList.add("row");
         let listpokemon = document.createElement("li");
-        listpokemon.addClass("list-group-item");
+        listpokemon.classList.add("list-group-item");
         let button = document.createElement("button");
         button.innerText = pokemon.name;
         button.classList.add("btn-primary");
