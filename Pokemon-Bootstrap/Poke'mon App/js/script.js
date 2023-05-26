6bcd2da3afef08415bcc51f9bf1d783d130dd3b5
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2023-05-25 21:31:21 -0400

    columns added to grid

--- Pokemon-Bootstrap/Poke'mon App/js/script.js
+++ Pokemon-Bootstrap/Poke'mon App/js/script.js
@@ -13,7 +13,9 @@ let pokemonRespository = (function () {
     }
     function addListItem(pokemon) {
         let pokemonList = document.querySelector(".list-group");
+        pokemonList.classList.add("row");
         let listpokemon = document.createElement("li");
+        listpokemon.classList.add("col-1");
         listpokemon.classList.add("list-group-item");
         let button = document.createElement("button");
         button.innerText = pokemon.name;
