7ee2b84b474b6d7ede36a4049410d41e6162bbac
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2023-05-25 20:22:48 -0400

    changing UL class in addList function

--- Pokemon-Bootstrap/Poke'mon App/js/script.js
+++ Pokemon-Bootstrap/Poke'mon App/js/script.js
@@ -12,7 +12,7 @@ let pokemonRespository = (function () {
         return pokemonList;
     }
     function addListItem(pokemon) {
-        let pokemonList = document.querySelector(".list-pokemon");
+        let pokemonList = document.querySelector(".list-group");
         pokemonList.classList.add("row");
         let listpokemon = document.createElement("li");
         listpokemon.classList.add("list-group-item");
