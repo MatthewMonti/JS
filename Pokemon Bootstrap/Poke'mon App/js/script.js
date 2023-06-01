68294c6a7e55e2e4d74ba59147d0aa99f6be58ee
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2023-05-31 23:43:05 -0400

    fix close issue

--- Pokemon Bootstrap/Poke'mon App/js/script.js
+++ Pokemon Bootstrap/Poke'mon App/js/script.js
@@ -106,12 +106,12 @@ let pokemonRespository = (function () {
         }
 
     function hideModal() {
-        let modalContainer = document.querySelector('#exampleModal');
+        let modalContainer = document.querySelector('.modal-dialog');
         modalContainer.classList.remove('is-visible');
 
     }
     window.addEventListener('keydown', (e) => {
-        let modalContainer = document.querySelector('#exampleModal');
+        let modalContainer = document.querySelector('.modal-dialog');
         if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
             hideModal();
         }
