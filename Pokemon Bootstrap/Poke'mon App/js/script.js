6deee9033336dec207ec99c6cfccdf3e82795299
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2023-05-31 20:04:42 -0400

    fix id issue

--- Pokemon Bootstrap/Poke'mon App/js/script.js
+++ Pokemon Bootstrap/Poke'mon App/js/script.js
@@ -22,7 +22,7 @@ let pokemonRespository = (function () {
         button.innerText = pokemon.name;
         button.classList.add("btn-primary");
         button.classList.toggle("modal");
-        button.setAttribute(id,"exampleModal");
+        button.setAttribute("id","exampleModal");
         listpokemon.appendChild(button);
         pokemonList.appendChild(listpokemon);
         addPokemonEventListener(button, pokemon);
