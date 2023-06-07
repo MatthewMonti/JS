c80b2aafc29826deeba22ce961bb862c03e26869
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2023-06-06 22:55:27 -0400

    fix toggle error

--- Pokemon Bootstrap/Poke'mon App/js/script.js
+++ Pokemon Bootstrap/Poke'mon App/js/script.js
@@ -17,7 +17,7 @@ let pokemonRespository = (function () {
         let listpokemon = document.createElement("li");
         listpokemon.classList.add("list-group-item");
         let button = document.createElement("button");
-        button.setAttribute(data-toggle,".modal");
+        button.classList.toggle(".modal");
         button.setAttribute(data-target,"#exampleModal")
         button.innerText = pokemon.name;
         button.classList.add("card-btn");
