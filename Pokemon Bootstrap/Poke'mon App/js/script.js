19fad998ef9867a1c526aabca1555a69dccf4447
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2023-06-06 22:44:36 -0400

    remove data-target

--- Pokemon Bootstrap/Poke'mon App/js/script.js
+++ Pokemon Bootstrap/Poke'mon App/js/script.js
@@ -19,7 +19,6 @@ let pokemonRespository = (function () {
         let button = document.createElement("button");
         button.innerText = pokemon.name;
         button.classList.add("card-btn");
-        button.setAtrribute(data-target,"#exampleModal")
         listpokemon.appendChild(button);
         pokemonList.appendChild(listpokemon);
         addPokemonEventListener(button, pokemon);
