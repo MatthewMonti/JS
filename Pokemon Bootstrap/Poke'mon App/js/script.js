a905d08d9d821c2bbc41bbda0b945ca50a5b057c
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2023-06-09 15:07:48 -0400

    col 11 fix

--- Pokemon Bootstrap/Poke'mon App/js/script.js
+++ Pokemon Bootstrap/Poke'mon App/js/script.js
@@ -17,14 +17,21 @@ let pokemonRespository = (function () {
 
         let pokedex_row1 = document.createElement("div");
         pokedex_row1.classList.add("row");
-        let pokedex_col = document.createElement("div");
-        pokedex_col.classList.add("col");
-        pokedex_row1.appendChild(pokedex_col);
-        pokedex_row1.appendChild(pokedex_col);
-        pokedex_row1.appendChild(pokedex_col);
-        pokedex_row1.appendChild(pokedex_col);
-        pokedex_row1.appendChild(pokedex_col);
-        pokedex_row1.appendChild(pokedex_col);   
+        let pokedex_col1 = document.createElement("div");
+        pokedex_col1.classList.add("col");  
+        let pokedex_col1 = document.createElement("div");
+        pokedex_col2.classList.add("col"); 
+        let pokedex_col2 = document.createElement("div");
+        pokedex_col3.classList.add("col"); 
+        let pokedex_col3 = document.createElement("div");
+        pokedex_col1.classList.add("col"); 
+        let pokedex_col4 = document.createElement("div");
+        pokedex_col4.classList.add("col"); 
+        let pokedex_col5 = document.createElement("div");
+        pokedex_col5.classList.add("col"); 
+        let pokedex_col6 = document.createElement("div");
+        pokedex_col6.classList.add("col"); 
+
         
         let listpokemon = document.createElement("li");
         listpokemon.classList.add("list-group-item");
@@ -37,8 +44,20 @@ let pokemonRespository = (function () {
 
         pokemon_deck.appendChild(listpokemon);
         pokemon_deck.appendChild(pokedex_row1);
-        pokedex_row1.appendChild(pokedex_col);
-        pokedex_col.appendChild(listpokemon);
+        pokedex_row1.appendChild(pokedex_col1);
+        pokedex_row1.appendChild(pokedex_col2);
+        pokedex_row1.appendChild(pokedex_col3);
+        pokedex_row1.appendChild(pokedex_col4);
+        pokedex_row1.appendChild(pokedex_col5);
+        pokedex_row1.appendChild(pokedex_col6);
+
+        pokedex_col1.appendChild(listpokemon);
+        pokedex_col2.appendChild(listpokemon);
+        pokedex_col3.appendChild(listpokemon);
+        pokedex_col4.appendChild(listpokemon);
+        pokedex_col5.appendChild(listpokemon);
+        pokedex_col6.appendChild(listpokemon);
+        
         listpokemon.appendChild(button);
         addPokemonEventListener(button, pokemon);
     }
