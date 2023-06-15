93d3918e98174c14f0db919cf2cc124f491f23c1
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2023-06-14 23:21:44 -0400

    update

--- Pokemon Bootstrap/Poke'mon App/src/script.js
+++ Pokemon Bootstrap/Poke'mon App/src/script.js
@@ -165,16 +165,3 @@ let pokemonRespository = (function () {
             pokemonRespository.addListItem(pokemon);
         });
     });
-
-    {
-        "env": {
-          "es6": true,
-          "browser": true
-        },
-        "extends": [
-          "eslint:recommended"
-        ],
-        "rules": {
-          "quotes": ["error", "single"]
-        }
-      }
