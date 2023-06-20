0f684a906ab8d5652793f738041d3596638af58f
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2023-06-20 19:54:28 -0400

    sticky navbar fix 2

--- src/script.js
+++ src/script.js
@@ -1,13 +1,8 @@
-// When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
 
-// Get the navbar
 var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
@@ -15,7 +10,6 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
-
 let pokemonRespository = (function () {
   let pokemonList = [];
   let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
