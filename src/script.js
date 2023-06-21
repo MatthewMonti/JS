0f22f0d646fecabe541321af455026efae09f7ef
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2023-06-21 13:26:12 -0400

    remove sticky js command w3

--- src/script.js
+++ src/script.js
@@ -1,15 +1,3 @@
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
 let pokemonRespository = (function () {
   let pokemonList = [];
   let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
